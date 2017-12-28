e2258d3b0060324ae2bdbc0218fef92e41c17406
cloessen <cloessen@gmail.com>
2017-12-28 07:46:15 +0100

    little display changes

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatIconModule} from '@angular/material';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatIconModule,
+  MatGridListModule, MatToolbarModule
+} from '@angular/material';
 
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule, MatGridListModule, MatToolbarModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatFormFieldModule, MatIconModule, MatGridListModule, MatToolbarModule],
 })
 export class MaterialModule { }
